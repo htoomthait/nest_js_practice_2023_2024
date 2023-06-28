51699ab395c41ee0b7b3b046fab532edc927ff09
Htoo Maung Thait <hmthait@blueplanetmm.com>
2023-06-28 09:32:26 +0630

    auth module in progress

--- nest_js_access_token_refresh_token_ultimate_guide/src/auth/auth.controller.ts
+++ nest_js_access_token_refresh_token_ultimate_guide/src/auth/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  private _authService: AuthService;
+
+  constructor(private authService: AuthService) {
+    this._authService = authService;
+  }
+  @Post('/loca/signup')
+  public signupLocal(@Body() authDto: AuthDto) {
+    return this._authService.signupLocal(authDto);
+  }
+
+  @Post('/loca/signin')
+  public signinLocal() {
+    return this._authService.signinLocal();
+  }
+
+  @Post('/logout')
+  public logout() {
+    return this._authService.logout();
+  }
+
+  @Post('/refresh')
+  public refreshTokens() {
+    return this._authService.refreshTokens();
+  }
+}
--- nest_js_access_token_refresh_token_ultimate_guide/src/auth/auth.service.ts
+++ nest_js_access_token_refresh_token_ultimate_guide/src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private _prismaService: PrismaService;
+
+  constructor(private prismaService: PrismaService) {
+    this._prismaService = prismaService;
+  }
+
+  public signupLocal(authDto: AuthDto) {
+    return 'This is signupLocal';
+  }
+
+  public signinLocal() {
+    return 'This is signinLocal';
+  }
+
+  public logout() {
+    return 'This is logout';
+  }
+
+  public refreshTokens() {
+    return 'This is refreshTokens';
+  }
+}
