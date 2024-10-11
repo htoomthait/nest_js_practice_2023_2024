9bd3153031a16da5abdfb2ab8033bd8ed1bbec75
Htoo Maung Thait <htoomaungthait@gmail.com>
2024-10-12 00:50:12 +0630

    migration file updated

--- racing_car_gallery/prisma/migrations/20241011181926_init/migration.sql
+++ racing_car_gallery/prisma/migrations/20241011181926_init/migration.sql
@@ -1,5 +1,13 @@
--- AlterTable
-ALTER TABLE `car_types` ADD COLUMN `status` ENUM('active', 'inactive') NOT NULL DEFAULT 'active';
+-- CreateTable
+CREATE TABLE `car_types` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `status` ENUM('active', 'inactive') NOT NULL DEFAULT 'active',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `car_brands` (
